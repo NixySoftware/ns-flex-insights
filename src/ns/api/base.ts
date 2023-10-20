577a786f1e8ef4c24ef4553130d43cff29498ad0
Danielle Huisman <danielle@huisman.me>
2023-10-20 17:35:18 +0200

    Improve API call function reuse

--- src/ns/api/base.ts
+++ src/ns/api/base.ts
@@ -4,6 +4,35 @@ export const API_URL = 'https://gateway.apiportal.ns.nl';
 
 const ajv = new Ajv();
 
+const apiCall = async <Parameters extends Record<string, string>>(
+    endpointUrl: string,
+    parameters?: Parameters,
+    init?: RequestInit
+) => {
+    const headers = new Headers(init?.headers);
+    headers.set('Ocp-Apim-Subscription-Key', process.env.NS_API_SUBSCRIPTION_KEY ?? '');
+
+    const url = new URL(`${API_URL}${endpointUrl}`);
+    if (parameters) {
+        for (const [key, value] of Object.entries(parameters)) {
+            url.searchParams.set(key, value);
+        }
+    }
+
+    const response = await fetch(url, {
+        ...init,
+        headers
+    });
+
+    const data = await response.json();
+
+    if (response.status >= 400 && response.status <= 599) {
+        // TODO: handle errors
+    }
+
+    return data;
+};
+
 export const generateApiCall = <Parameters extends Record<string, string>, Schema extends Record<string, unknown>>(
     endpointUrl: string,
     schema: Schema
@@ -11,26 +40,7 @@ export const generateApiCall = <Parameters extends Record<string, string>, Schem
     const validate = ajv.compile(schema);
 
     return async (parameters?: Parameters, init?: RequestInit): Promise<JTDDataType<Schema>> => {
-        const headers = new Headers(init?.headers);
-        headers.set('Ocp-Apim-Subscription-Key', process.env.NS_API_SUBSCRIPTION_KEY ?? '');
-
-        const url = new URL(`${API_URL}${endpointUrl}`);
-        if (parameters) {
-            for (const [key, value] of Object.entries(parameters)) {
-                url.searchParams.set(key, value);
-            }
-        }
-
-        const response = await fetch(url, {
-            ...init,
-            headers
-        });
-
-        const data = await response.json();
-
-        if (response.status >= 400 && response.status <= 599) {
-            // TODO: handle errors
-        }
+        const data = await apiCall(endpointUrl, parameters, init);
 
         if (!validate(data)) {
             // TODO
